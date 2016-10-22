6cd30db3edead297e25a989377f57b59fadec4a7
Wojciech <wojciech.dziwulski@gmail.com>
2016-10-22 15:24:31 +0800

    Added my pairing function to the Story class

--- src/Story.py
+++ src/Story.py
@@ -2,8 +2,53 @@ from itertools import *
 import numpy as np
 import copy
 
-def uniqueMapping(occurenceIndices):
-    return len(occurenceIndices)
+def uniqueMapping(indexes):
+    if len(indexes) == 0:
+        a = 0
+        b = 0
+    elif len(indexes) == 1:
+        a = indexes[0]
+        b = 0
+    elif len(indexes) == 2:
+        a = indexes[0]
+        b = indexes[1]
+    else:
+        a = indexes[0]
+        b = uniqueMapping(indexes[1:])
+
+    output = cantorMapping(a,b)
+    return output
+
+def reverseUniqueMapping(mapping):
+    a, b = reverseCantorMapping(mapping)
+    indexes = [int(a)]
+
+    # threshold - basically the number of elements in the array
+    threshold = 32
+
+    if b > threshold:
+        indexes += reverseUniqueMapping(b)
+    else:
+        indexes.append(int(b))
+
+    return indexes
+
+def cantorMapping(a,b):
+    output = (a+b)*(a+b+1)/2+b
+
+    return output
+
+def reverseCantorMapping(mapping):
+    w = math.floor((math.sqrt(8*mapping+1)-1)/2)
+    t = w*(w+1)/2
+
+    b = mapping-t
+    a = w-b
+
+    returnList = [a,b]
+    returnList.sort()
+
+    return returnList
 
 class Story:
     def __init__(self):
@@ -131,7 +176,9 @@ class Story:
                 else:
                     ansNumbers.append(dictionary.index(w))
             # Make a unique mapping from the indices to an integer
-            labels[i] = uniqueMapping(ansNumbers)
+            # uniqueMapping not active yet because we're scared of the big noise
+            # labels[i] = uniqueMapping(ansNumbers)
+            labels[i] = ansNumbers[0]
         return labels
 
 
