ea1a0b49c4ad493836c67141ff980fa90a022fa4
Wojciech <wojciech.dziwulski@gmail.com>
2016-10-31 01:53:28 +0800

    Used unique mapping with bgr

--- src/parser.py
+++ src/parser.py
@@ -52,86 +52,89 @@ def parseFile(filename, hasAnswers = True):
 def parseAll():
     print ("Parsing the train data...")
 
-    stories, dictionary, bigrams = parseFile(KAGGLE_TRAIN_TXT_FILE)
-    trainData_um = np.zeros([1,len(dictionary) - 1]) # with uniqueMapping
-    trainData_bow = np.zeros([1,len(dictionary) - 1]) # as bag-of-words
-    trainData_bgr = np.zeros([1,len(bigrams)]) # as bag-of-words
+    # stories, dictionary, bigrams = parseFile(KAGGLE_TRAIN_TXT_FILE)
+    stories = np.load("../data/train_stories.npy")
+    dictionary = np.load("../data/dictionary.npy")
+    bigrams = np.load("../data/bigrams.npy")
+    # trainData_um = np.zeros([1,len(dictionary) - 1]) # with uniqueMapping
+    # trainData_bow = np.zeros([1,len(dictionary) - 1]) # as bag-of-words
+    # trainData_bgr = np.zeros([1,len(bigrams)]) # as bag-of-words
     trainData_bgr_order = np.zeros([1,len(bigrams)]) # as bag-of-words
-    trainLabels = np.zeros([1, len(dictionary)])
+    # trainLabels = np.zeros([1, len(dictionary)])
 
     print ("Producing the train data points and labels...")
 
     # Make points and labels for each
     for story in stories:
-        points_um = story.constructPoints(dictionary[1:], bagOfWords=False)
-        points_bow = story.constructPoints(dictionary[1:], bagOfWords=True)
-        points_bgr = story.constructBigramPoints(bigrams)
+        # points_um = story.constructPoints(dictionary[1:], bagOfWords=False)
+        # points_bow = story.constructPoints(dictionary[1:], bagOfWords=True)
+        # points_bgr = story.constructBigramPoints(bigrams)
         points_bgr_order = story.constructBigramOrderPoints(bigrams)
 
-        trainData_um = np.concatenate((trainData_um, points_um), axis = 0)
-        trainData_bow = np.concatenate((trainData_bow, points_bow), axis = 0)
-        trainData_bgr = np.concatenate((trainData_bgr, points_bgr), axis = 0)
-        trainData_bgr_order = np.concatenate((trainData_bgr, points_bgr_order), axis = 0)
+        # trainData_um = np.concatenate((trainData_um, points_um), axis = 0)
+        # trainData_bow = np.concatenate((trainData_bow, points_bow), axis = 0)
+        # trainData_bgr = np.concatenate((trainData_bgr, points_bgr), axis = 0)
+        trainData_bgr_order = np.concatenate((trainData_bgr_order, points_bgr_order), axis = 0)
 
-        labels = story.constructBinaryLabels(dictionary)
-        trainLabels = np.concatenate((trainLabels, labels), axis = 0)
+        # labels = story.constructBinaryLabels(dictionary)
+        # trainLabels = np.concatenate((trainLabels, labels), axis = 0)
 
-    trainData_um = trainData_um[1:]
-    trainData_bow = trainData_bow[1:]
-    trainData_bgr = trainData_bgr[1:]
+    # trainData_um = trainData_um[1:]
+    # trainData_bow = trainData_bow[1:]
+    # trainData_bgr = trainData_bgr[1:]
     trainData_bgr_order = trainData_bgr_order[1:]
 
-    trainLabels = trainLabels[1:]
+    # trainLabels = trainLabels[1:]
 
     print ("Parsing the test data...")
 
-    testStories, _, _ = parseFile(KAGGLE_TEST_TXT_FILE, hasAnswers=False)
-    testData_um = np.zeros([1,len(dictionary) - 1])
-    testData_bow = np.zeros([1,len(dictionary) - 1])
-    testData_bgr = np.zeros([1,len(bigrams)])
+    # testStories, _, _ = parseFile(KAGGLE_TEST_TXT_FILE, hasAnswers=False)
+    testStories = np.load("../data/test_stories.npy")
+    # testData_um = np.zeros([1,len(dictionary) - 1])
+    # testData_bow = np.zeros([1,len(dictionary) - 1])
+    # testData_bgr = np.zeros([1,len(bigrams)])
     testData_bgr_order = np.zeros([1,len(bigrams)])
-
-    testLabels = np.zeros([1, len(dictionary)])
+    # testLabels = np.zeros([1, len(dictionary)])
 
     print ("Producing the test data points and labels...")
 
     for story in testStories:
-        points_um = story.constructPoints(dictionary[1:], bagOfWords=False)
-        points_bow = story.constructPoints(dictionary[1:], bagOfWords=True)
-        points_bgr = story.constructBigramPoints(bigrams)
+        # points_um = story.constructPoints(dictionary[1:], bagOfWords=False)
+        # points_bow = story.constructPoints(dictionary[1:], bagOfWords=True)
+        # points_bgr = story.constructBigramPoints(bigrams)
         points_bgr_order = story.constructBigramOrderPoints(bigrams)
 
-        testData_um = np.concatenate((testData_um, points_um), axis = 0)
-        testData_bow = np.concatenate((testData_bow, points_bow), axis = 0)
-        testData_bgr = np.concatenate((testData_bgr, points_bgr), axis = 0)
+        # testData_um = np.concatenate((testData_um, points_um), axis = 0)
+        # testData_bow = np.concatenate((testData_bow, points_bow), axis = 0)
+        # testData_bgr = np.concatenate((testData_bgr, points_bgr), axis = 0)
         testData_bgr_order = np.concatenate((testData_bgr_order, points_bgr_order), axis=0)
 
-        labels = story.constructBinaryLabels(dictionary)
-        testLabels = np.concatenate((testLabels, labels), axis = 0)
+        # labels = story.constructBinaryLabels(dictionary)
+        # testLabels = np.concatenate((testLabels, labels), axis = 0)
 
-    testData_um = testData_um[1:]
-    testData_bow = testData_bow[1:]
-    testData_bgr = testData_bgr[1:]
+    # testData_um = testData_um[1:]
+    # testData_bow = testData_bow[1:]
+    # testData_bgr = testData_bgr[1:]
     testData_bgr_order = testData_bgr_order[1:]
 
-    testLabels = testLabels[1:]
+    # testLabels = testLabels[1:]
 
 
     # Save everything to files
     print("Saving parsed data to .npy files")
-    np.save(DICTIONARY_FILE, dictionary)
-    np.save(BIGRAMS_FILE, bigrams)
+    # np.save(DICTIONARY_FILE, dictionary)
+    # np.save(BIGRAMS_FILE, bigrams)
 
-    np.save(TRAIN_DATA_UM_FILE, trainData_um)
-    np.save(TRAIN_DATA_BOW_FILE, trainData_bow)
-    np.save(TRAIN_DATA_BGR_FILE, trainData_bgr)
+    # np.save(TRAIN_DATA_UM_FILE, trainData_um)
+    # np.save(TRAIN_DATA_BOW_FILE, trainData_bow)
+    # np.save(TRAIN_DATA_BGR_FILE, trainData_bgr)
     np.save(TRAIN_DATA_BGR_ORDER_FILE, trainData_bgr_order)
-    np.save(TRAIN_LABELS_FILE, trainLabels)
-    np.save(TRAIN_STORIES_FILE, np.array(stories))
+    # np.save(TRAIN_LABELS_FILE, trainLabels)
+    # np.save(TRAIN_STORIES_FILE, np.array(stories))
 
-    np.save(TEST_DATA_UM_FILE, testData_um)
-    np.save(TEST_DATA_BOW_FILE, testData_bow)
+    # np.save(TEST_DATA_UM_FILE, testData_um)
+    # np.save(TEST_DATA_BOW_FILE, testData_bow)
     np.save(TEST_DATA_BGR_ORDER_FILE, testData_bgr_order)
-    np.save(TEST_STORIES_FILE, np.array(testStories))
+    # np.save(TEST_STORIES_FILE, np.array(testStories))
 
 # parseAll()
